445e30d997fce3711b376c1dc483a2ddffb994e0
Nick <Pierce.nick24@gmail.com>
2025-05-05 19:16:25 -0400

    Update calculator.py
    added closing sequences with time

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+import time 
 #Solve
 def add(a, b):
     answer = a + b
@@ -31,25 +32,37 @@ while True:
         a = int(input("\nInput First Number: "))
         b = int(input("Input Second Number: "))
         add(a, b)
+        time.sleep(3)
 
     elif choice == "b" or choice == "B":
         print("Using Subtraction")
         a = int(input("\nInput First Number: "))
         b = int(input("Input Second Number: "))
         sub(a, b)
+        time.sleep(3)
 
     elif choice == "c" or choice == "C":
         print("Using Multiplication")
         a = int(input("\nInput First Number: "))
         b = int(input("Input Second Number: "))
         mul(a, b)
+        time.sleep(3)
 
     elif choice == "d" or choice == "D":
         print("Using Division")
         a = int(input("\nInput First Number: "))
         b = int(input("Input Second Number: "))
         div(a, b)
+        time.sleep(3)
 
     elif choice == "e" or choice == "E":
         print("\nThe Program Has Ended")
+        print("closing in...")
+        time.sleep(1)
+        print("3")
+        time.sleep(1)
+        print("2")
+        time.sleep(1)
+        print("1")
+        time.sleep(1)
         quit()
